269055e5bc9426151777140c0e0c9bdd70015abd
caddamss2025 <caddamss2025@gmail.com>
2026-05-09 15:35:29 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
-    allowedHosts: ['inadequately-unimpearled-alline.ngrok-free.dev'],
+    port: 5173
     proxy: {
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'http://localhost:8080',
